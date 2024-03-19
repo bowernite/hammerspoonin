318e431a1b82ae9ed1abd48396c71317b504062b
Brett <bjabramczyk@gmail.com>
2024-03-19 12:30:09 -0500

    flux and vivid compatibility

--- vivid_fix.lua
+++ vivid_fix.lua
@@ -6,19 +6,22 @@ local function isProcessRunning(processName)
     return output ~= ""
 end
 
-local function restartVividApp()
-    log("Checking if Vivid app is running")
-    if not isProcessRunning("Vivid") then
-        log("Vivid app not running")
-    else
-        log("Vivid app is running; killing")
-      
-        hs.execute("pkill -f 'Vivid'")
+local function killProcess(processName)
+    log("🔆 Checking if " .. processName .. " is running")
+    if isProcessRunning(processName) then
+        log("🔆 " .. processName .. " is running; killing")
+        hs.execute("pkill -f '" .. processName .. "'")
         -- Wait for the app to fully terminate before attempting to restart
         hs.timer.usleep(500000) -- 0.5 seconds
+    else
+        log("🔆 " .. processName .. " not running")
     end
-    
-    log("Starting Vivid App")
+end
+
+local function restartVividApp()
+    killProcess("Vivid")
+
+    log("🔆 Starting Vivid App")
     hs.application.open("/Applications/Vivid.app")
 end
 
@@ -36,3 +39,41 @@ screenWatcher:start()
 
 -- Delay initial restart to prevent potential loop at startup
 hs.timer.doAfter(1, function() restartVividApp() end)
+
+local function ensureFluxRunning()
+    local sunsetTime = hs.location.sunset() -- Get sunset time
+    local sunriseTime = hs.location.sunrise() -- Get sunrise time
+
+    -- Adjust times to 30 minutes after sunset and 30 minutes before sunrise
+    local fluxStartTime = os.date("*t", sunsetTime + (30 * 60))
+    local fluxEndTime = os.date("*t", sunriseTime - (30 * 60))
+
+    local currentTime = os.time()
+
+    -- Check if current time is within the Flux running period
+    if currentTime > fluxStartTime and currentTime < fluxEndTime then
+        if not isProcessRunning("Flux") then
+            log(
+                "🔆🕯️ Flux is not running during its allowed time; starting Flux")
+            hs.execute("open -a Flux")
+        else
+            log("🔆🕯️ Flux is already running as expected")
+        end
+    else
+        if isProcessRunning("Flux") then
+            log(
+                "🔆🕯️ Flux is running outside its allowed time; killing Flux")
+            killProcess("Flux")
+            -- Restart Vivid app whenever we kill Flux
+            restartVividApp()
+        else
+            log(
+                "🔆🕯️ Flux is not running outside its allowed time, as expected")
+        end
+    end
+end
+
+-- Check Flux status every minute to ensure it's running or killed as per the schedule
+fluxTimer = hs.timer.doEvery(60, ensureFluxRunning)
+ensureFluxRunning()
+
