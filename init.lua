6e1f9560b4dd3d1b222b62801fad1ff50b0fc640
Brett <bjabramczyk@gmail.com>
2024-03-26 15:06:18 -0500

    turn off window management

--- init.lua
+++ init.lua
@@ -3,7 +3,8 @@ hs.console.consoleFont({name = "Menlo", size = 18})
 
 hs.ipc.cliInstall()
 
-require("windows/window_management")
+-- This is a rabbit hole, and you ain't ready right now
+-- require("windows/window_management")
 -- Doesn't seem to help err_network_changed..?
 -- require("disconnect_from_wifi_when_on_ethernet")
 require("vivid_fix")
